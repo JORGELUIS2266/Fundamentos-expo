48aa55b559e03a9a2a26b8e60d4b040bcc6e1bbf
Jorge <yo.hemati2322@gmail.com>
2025-09-14 20:20:22 -0600

    Creando Navbar

--- src/pages/Lista-Alumnos/ListaAlumnos.js
+++ src/pages/Lista-Alumnos/ListaAlumnos.js
@@ -1,45 +1,54 @@
 import { StyleSheet, View, Platform, StatusBar, Text, TextInput, ScrollView, TouchableOpacity, Image, Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome } from '@expo/vector-icons';
 
-import Icon from 'react-native-vector-icons/FontAwesome';
 export default function ListaAlumnos() {
     return (
         <SafeAreaView style={styles.main}>
             {/* Inicio de AppBar */}
             <View style={styles.appBar}>
-                <Icon name="arrow-left" zize={20} color={'#000'} />
-                <Text style = {styles.appBarTitle}>
+                <FontAwesome name="arrow-left" size={20} color={'#000'} />
+                <Text style={styles.appBarTitle}>
                     Lista de Usuarios
                 </Text>
-                <View style = {{width: 20}}></View>
-
-                
+                <View style={{width: 20}}></View>
             </View>
             {/* Cierre de AppBar */}
 
             {/* Inicio de contenido(Lista de alumnos) */}
             <ScrollView style={{padding:16}}>
                 <Text style={styles.sectionTitle} >Alumnos de Aplicaciones Moviles</Text>
-                 {[1,2,3,4,5,6,7].map(()  => (
-                     <View style={styles.card}> 
-                     <Image source={{uri:'https://static.wikia.nocookie.net/patoaventuras/images/f/fa/PatoDonald%282017%29.png/revision/latest?cb=20180421215810&path-prefix=es'}} style={styles.avatar}/>
-                     <View style={styles.cardInfo}>
-                        <Text style={styles.UserName} >Jorge Luis Hernandez Matra</Text>
-                        <Text style={styles.UserDetails} >ING. Sistemas Computacionales</Text>
-                        <TouchableOpacity style={styles.saveButton}>
-                            <Text style={styles.saveButtonText}>Ver mas</Text>
-                        </TouchableOpacity>
-                     </View>
-
-                
-                </View>
+                 {[1,2,3,4,5,6,7].map((item, index)  => (
+                     <View style={styles.card} key={index}> 
+                         <Image source={{uri:'https://static.wikia.nocookie.net/patoaventuras/images/f/fa/PatoDonald%282017%29.png/revision/latest?cb=20180421215810&path-prefix=es'}} style={styles.avatar}/>
+                         <View style={styles.cardInfo}>
+                            <Text style={styles.UserName} >Jorge Luis Hernandez Matra</Text>
+                            <Text style={styles.UserDetails} >ING. Sistemas Computacionales</Text>
+                            <TouchableOpacity style={styles.saveButton}>
+                                <Text style={styles.saveButtonText}>Ver mas</Text>
+                            </TouchableOpacity>
+                         </View>
+                    </View>
                  ))}
-
-               
-                
-                
             </ScrollView>
             {/* cierre de contenido (Lista de Alumnos) */}
+
+            {/*navbar  */}
+            <View style={styles.navbar}>
+                <View style={styles.navItem}>
+                    <FontAwesome name="home" size={20} color={'#000'} />
+                    <Text style={styles.navText}>Inicio</Text>
+                </View>
+                <View style={styles.navItem}>
+                    <FontAwesome name="save" size={20} color={'#000'} />
+                    <Text style={styles.navText}>Guardar</Text>
+                </View>
+                <View style={styles.navItem}>
+                    <FontAwesome name="user" size={20} color={'#000'} />
+                    <Text style={styles.navText}>Usuario</Text>
+                </View>
+            </View>
+            {/* fin navbar */}
         </SafeAreaView>
     );
 }
@@ -120,6 +129,26 @@ const styles = StyleSheet.create({
         fontSize: 17,
         color: '#ffffffff',
     },
+    navbar:{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        height:70,
+        backgroundColor: '#d284ffff',
+        borderTopWidth: 1,
+        // marginBottom:10,
+
+    },
+    navItem:{
+        alignItems: 'center',
+
+    },
+    navText:{
+        fontSize: 12,
+        color: '#000000ff',
+        marginTop:4,
+    },
+
     
 
 
